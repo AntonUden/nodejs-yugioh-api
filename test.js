91c40ecf4e1e0da8376978df3e0ea20e3481e704
Zeeraa <udenanton@hotmail.com>
2019-02-23 22:18:27 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,19 @@
+var ygoapi = require('./yugioh-api.js');
+
+console.log('\nygoapi.getStatus():\n');
+console.log(JSON.stringify(ygoapi.getStatus(), null, 4));
+
+console.log('\nygoapi.getCardInfo(\'sp15-en038\'):\n');
+console.log(JSON.stringify(ygoapi.getCardInfo('sp15-en038'), null, 4));
+
+
+console.log('\nygoapi.getCardInfo(\'ligma\'):\n');
+console.log(JSON.stringify(ygoapi.getCardInfo('ligma'), null, 4));
+
+
+console.log('\nygoapi.getCardPrice(\'sp15-en038\'):\n');
+console.log(JSON.stringify(ygoapi.getCardPrice('sp15-en038'), null, 4));
+
+
+console.log('\nygoapi.getCardPrice(\'ligma\'):\n');
+console.log(JSON.stringify(ygoapi.getCardPrice('ligma'), null, 4));
---
+++ yugioh-api.js
@@ -0,0 +1,144 @@
+var request = require('sync-request');
+var cheerio = require('cheerio');
+var fakeUa = require('fake-useragent');
+var querystring = require('querystring');
+
+exports.getCardPrice = function(printTag) {
+	printTag = printTag.toUpperCase();
+	let result = new Object();
+
+	let cardInfo = this.getCardInfo(printTag);
+
+	if(cardInfo.success) {
+		result.card = cardInfo.data;
+		result.found_yugiohprices = true;
+		response = request('GET', 'https://www.cardmarket.com/en/YuGiOh/Cards/' + querystring.escape(cardInfo.data.name.replace(/\s+/g, '-')) + '/Versions', {
+			headers: {
+				'user-agent': fakeUa()
+			},
+		});
+		if(response.statusCode == 200) {
+			let $ = cheerio.load(response.getBody('utf8'));
+			let versions = $('#ReprintSection').find('.card-column');
+			let URLs = [];
+
+			for(let i = 0; i < versions.length; i++) {
+				let url = 'https://www.cardmarket.com' + $(versions[i]).find('a').first().attr('href');
+				let expansion = $(versions[i]).find('.yugiohExpansionIcon').text();
+				
+				if(printTag.startsWith(expansion.toUpperCase())) {
+					URLs.push(url);
+				}
+			}
+
+			let httpError = false;
+
+			for (let i = 0; i < URLs.length; i++) {
+				response = request('GET', URLs[i], {
+					headers: {
+						'user-agent': fakeUa()
+					},
+				});
+
+				if(response.statusCode == 200) {
+					$ = cheerio.load(response.getBody('utf8'));
+
+					if($('#tabContent-info').find('.icon').first().attr('onmouseover').toUpperCase().includes(cardInfo.data.price_data.rarity.toUpperCase())) {
+						
+						
+						break;
+					}
+				} else {
+					httpError = true;
+				}
+			}
+
+			console.log(URLs);
+		} else {
+			result.success = false;
+			result.error = true;
+			result.error_yugiohprices = false;
+			result.error_cardmarket = true;
+			result.found_yugiohprices = true;
+			result.found_cardmarket = false;
+			result.message = 'cardmarket.com statusCode: ' + response.statusCode;
+		}
+	} else {
+		if(cardInfo.error) {
+			result.success = false;
+			result.error = true;
+			result.error_yugiohprices = true;
+			result.found_yugiohprices = false;
+			result.message = cardInfo.message;
+		} else {
+			result.success = false;
+			result.error = false;
+			result.error_yugiohprices = false;
+			result.found_yugiohprices = false;
+			result.message = cardInfo.message;
+		}
+	}
+
+	return result;
+};
+
+exports.getCardInfo = function(printTag) {
+	printTag = printTag.toUpperCase();
+	let result = new Object();
+
+	let response = request('GET', 'https://yugiohprices.com/api/price_for_print_tag/' + printTag, {
+		headers: {
+			'user-agent': fakeUa()
+		},
+	});
+	
+	if(response.statusCode == 200) {
+		var data = JSON.parse(response.getBody('utf8'));
+		if(data.status == 'success') {
+			result.success = true;
+			result.error = false;
+			result.message = '';
+			result.data = data.data;
+		} else {
+			result.success = false;
+			result.error = false;
+			result.yugiohprices_status = data.status;
+			result.message = data.message;
+		}
+	} else {
+		result.success = false;
+		result.error = true;
+		result.message = 'yugiohprices.com statusCode: ' + response.statusCode;
+		result.statusCode = response.statusCode;
+	}
+
+	return result;
+};
+
+exports.getStatus = function(printTag) {
+	let result = new Object()
+	result.yugiohprices = new Object();
+	result.cardmarket = new Object();
+
+	let response = undefined;
+
+	// Random card from yugiohprices.com
+	response = request('GET', 'https://yugiohprices.com/api/price_for_print_tag/LCGX-EN016', {
+		headers: {
+			'user-agent': fakeUa()
+		},
+	});
+	
+	result.yugiohprices.working = (response.statusCode == 200);
+	result.yugiohprices.statusCode = response.statusCode;
+
+	response = request('GET', 'https://www.cardmarket.com/en/YuGiOh/Cards/Polymerization/Versions', {
+		headers: {
+			'user-agent': fakeUa()
+		},
+	});
+	result.cardmarket.working = (response.statusCode == 200);
+	result.cardmarket.statusCode = response.statusCode;
+	
+	return result;
+};
